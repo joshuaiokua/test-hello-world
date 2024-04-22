2fe7386912df48a3325b249abfe507e94394dccb
joshuaiokua <jiokua.albano@gmail.com>
2024-04-22 00:17:49 -0700

    Update hello-reqs.py

--- hello-reqs.py
+++ hello-reqs.py
@@ -1,3 +1,4 @@
 import os
+import requests
 
-print(f"Hello from {os.path.basename(__file__)}!")
+print(f"Hello from {os.path.basename(__file__)}! I have requests version {requests.__version__} installed.")
